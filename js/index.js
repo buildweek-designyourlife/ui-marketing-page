dc18648933d047bc1647290f5cde645a3bdf6807
Clif <clifhodges13@gmail.com>
2019-07-29 19:41:58 -0500

    finished up nav functionality & animations. remove nav on link click and outside click

--- js/index.js
+++ js/index.js
@@ -1,19 +1,46 @@
+// Resonsive Menu with mobile Hamburger
+
 const navSlide = () => {
   const burger = document.querySelector('.burger');
   const nav = document.querySelector('.nav-links');
-  const navLinks = document.querySelectorAll('.link');
+  const navLinks = document.querySelectorAll('.nav-links .link');
   console.log(navLinks);
+
+  //init function
+  function init() {
+    burger.classList.remove('toggle');
+    nav.classList.remove('nav-active');
+    navLinks.forEach(link => {link.style.animation = ''});
+  };
 
   // Toggle nav
   burger.addEventListener('click', () => {
     nav.classList.toggle('nav-active');
-    console.log('event registered')
-  });
 
-  //Animate links
-  navLinks.forEach((link, index) => {
-    link.style.animation = `navLinkFade 1s ease forwards ${index / 7 + 1.5}s`;
-    console.log(index);
+    //Animate links
+    navLinks.forEach((link, index) => {
+      if (link.style.animation) {
+        link.style.animation = '';
+      } else {
+        link.style.animation = `navLinkFade .35s ease forwards ${index / 7 + .25}s`;
+      }
+    });
+
+    // Burger animation
+    burger.classList.toggle('toggle');
+
+    // Slide nav off on link click
+    navLinks.forEach(link => link.addEventListener('click', () => {
+      // nav.classList.remove('nav-active');
+      init();
+    }))
+
+    // Slide nav off when clicking outside of the nav
+    window.addEventListener('mouseup', event => {
+      if (event.target !== nav) {
+        init();
+      }
+    })
   });
 }
 
