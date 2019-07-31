36dc07f41511f33b2bb9a799df61b43f635b0343
Clif <clifhodges13@gmail.com>
2019-07-30 20:45:50 -0500

    working on scroll animations

---
+++ js/scroll.js
@@ -0,0 +1,21 @@
+let tl = new TimelineMax({onUpdate:updatePercentage});
+const controller = new ScrollMagic.Controller();
+
+tl.from('.step', 15, {
+  x: 200,
+  opacity: 0
+});
+
+const scene = new ScrollMagic.Scene({
+  triggerElement: '.sticky',
+  triggerHook: 'onLeave',
+  duration: '100%'
+})
+  .setPin('.sticky')
+  .setTween(tl)
+  .addTo(controller);
+
+function updatePercentage() {
+  tl.progress();
+  console.log(tl.progress());
+}
